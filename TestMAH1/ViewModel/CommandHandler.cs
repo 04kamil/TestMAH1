c6c777b7861d69b9a0abf67a7d6911beced1a1c2
Kamil Brożek <kamil.brozek@hotmail.com>
2017-12-10 00:11:56 +0100

    Add project files.

---
+++ TestMAH1/ViewModel/CommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMAH1.ViewModel
+{
+    public class CommandHandler : System.Windows.Input.ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+        private Action _action;
+        private Func<bool> _canExecute;
+
+        public CommandHandler(Action action,Func<bool> canExecute)
+        {
+            _action = action;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return _canExecute();
+        }
+
+        public void Execute(object parameter)
+        {
+            _action();
+        }
+    }
+}
---
+++ TestMAH1/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using TestMAH1.View;
+
+
+namespace TestMAH1.ViewModel
+{
+    class MainWindowViewModel : INotifyPropertyChanged
+    {
+
+
+        private UserControl _MainPage;
+        public UserControl MainPage
+        {
+            get
+            {
+                return _MainPage;
+            }
+            set
+            {
+                OnPropertyChanged("MainPage");
+                _MainPage = value;
+            }
+        }
+
+        private ICommand _ExecuteCommand;
+        public ICommand ExecuteCommand
+        {
+            get
+            {
+                return _ExecuteCommand;
+
+            }
+
+            set
+            {
+                _ExecuteCommand = value;
+            }
+        }
+
+        private ICommand _HomePage;
+        public ICommand HomePage
+        {
+            get
+            {
+                return _HomePage;
+
+            }
+
+            set
+            {
+                _HomePage = value;
+            }
+        }
+
+        private ICommand _CreateModel;
+        public ICommand CreateModel
+        {
+            get
+            {
+                return _CreateModel;
+
+            }
+
+            set
+            {
+                _CreateModel = value;
+            }
+        }
+
+
+        public MainWindowViewModel()
+        {
+            ExecuteCommand = new CommandHandler(Execute, () => true);
+            HomePage = new CommandHandler(SetHomePage, () => true);
+            CreateModel = new CommandHandler(SetCreateModelPage, () => true);
+        }
+
+        private void Execute()
+        {
+            MessageBox.Show("Its life");
+        }
+
+        public void SetHomePage()
+        {
+            MainPage = new HomeUserControl();
+        }
+
+        public void SetCreateModelPage()
+        {
+            MainPage = new CreateModelUserControl();
+        }
+        #region Property Changed
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if(handler!=null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+        #endregion
+
+    }
+
+
+}
